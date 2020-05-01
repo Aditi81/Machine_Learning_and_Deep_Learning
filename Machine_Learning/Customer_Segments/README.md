bca22230d681e2fd2b98def0f09f11bb41dc12ba
Aditi81 <adi.kothiya@gmail.com>
2020-05-01 22:36:11 +0530

    initial commit for machine learning

---
+++ Machine_Learning/Customer_Segments/README.md
@@ -0,0 +1,24 @@
+# Content: Unsupervised Learning
+## Project: Identify Customer Segments
+
+### Install
+
+This project requires **Python 3.6** and the following Python libraries installed:
+
+- [NumPy](http://www.numpy.org/)
+- [Pandas](http://pandas.pydata.org)
+- [matplotlib](http://matplotlib.org/)
+- [scikit-learn](http://scikit-learn.org/stable/)
+
+You will also need to have software installed to run and execute a [Jupyter Notebook](http://ipython.org/notebook.html)
+
+If you do not have Python installed yet, it is highly recommended that you install the [Anaconda](http://continuum.io/downloads) distribution of Python, which already has the above packages and more included. Make sure that you select the Python 2.7 installer and not the Python 3.x installer. 
+
+### Data Origin
+The files used in the project:
+
+Udacity_AZDIAS_Subset.csv : Demographic data for the general population of Germany; 891211 persons (rows) x 85 features (columns).
+Udacity_CUSTOMERS_Subset.csv : Demographic data for customers of a mail-order company; 191652 persons (rows) x 85 features (columns).
+Data_Dictionary.md : Information file about the features in the provided datasets.
+AZDIAS_Feature_Summary.csv : Summary of feature attributes for demographic data.
+Identify_Customer_Segments.ipynb : Jupyter Notebook divided into sections and guidelines for completing the project. The notebook provides more details and tips than the outline given here.
---
+++ Machine_Learning/Image_Classifier_PyTorch/predict_args.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+""" train_args.py
+Part 2 of the Udacity AIPND final project submission.
+predict_args.py contains the command line argument definitions for predict.py
+"""
+
+import argparse
+
+
+def get_args():
+    """
+    Get argument parser for train cli.
+
+    Command line argument examples:
+    - Set directory to save checkpoints: python train.py data_dir --save_dir save_directory
+    - Choose architecture: python train.py data_dir --arch "vgg13"
+    - Set hyperparameters: python train.py data_dir --learning_rate 0.01 --hidden_units 512 --epochs 20
+    - Use GPU for training: python train.py data_dir --gpu
+
+    For argparse examples see https://pymotw.com/3/argparse
+    Returns an argparse parser.
+    """
+
+    parser = argparse.ArgumentParser(
+        description="Image prediction.",
+        usage="python ./predict.py /path/to/image.jpg checkpoint.pth",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument('path_to_image',
+                        help='Path to image file.',
+                        action="store")
+
+    parser.add_argument('checkpoint_file',
+                        help='Path to checkpoint file.',
+                        action="store")
+
+    parser.add_argument('--save_dir',
+                        action="store",
+                        default=".",
+                        dest='save_dir',
+                        type=str,
+                        help='Directory to save training checkpoint file',
+                        )
+
+    parser.add_argument('--top_k',
+                        action="store",
+                        default=5,
+                        dest='top_k',
+                        type=int,
+                        help='Return top KK most likely classes.',
+                        )
+
+    parser.add_argument('--category_names',
+                        action="store",
+                        default="cat_to_name.json",
+                        dest='categories_json',
+                        type=str,
+                        help='Path to file containing the categories.',
+                        )
+
+    parser.add_argument('--gpu',
+                        action="store_true",
+                        dest="use_gpu",
+                        default=False,
+                        help='Use GPU')
+
+    parser.parse_args()
+    return parser
+
+
+def main():
+    """
+        Main Function
+    """
+    print(f'Command line argument utility for predict.py.\nTry "python train.py -h".')
+
+
+if __name__ == '__main__':
+    main()
+"""
+ main() is called if script is executed on it's own.
+"""
